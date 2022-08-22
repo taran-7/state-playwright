570efcf30993aaf1aa3d5d00a822f2be76e0a669
taran-7 <taran76@gmail.com>
2022-08-22 10:26:56 +0300

    up

--- tests/checkSite.spec.js
+++ tests/checkSite.spec.js
@@ -1,32 +1,57 @@
 // @ts-check
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Login,  Log out', async ({page}) => {
+test('Login,  Log out', async ({ page }) => {
     await page.goto('https://vdc-test.xelon.ch/');
     await page.waitForURL('https://vdc-test.xelon.ch/login')
+    // @ts-ignore
     await page.locator('[data-cy="email-input"]').fill(process.env.USERNAME);
+    // @ts-ignore
     await page.locator('[data-cy="password-input"]').fill(process.env.PASSWORD);
     const buttonLogin = page.locator('[data-cy="login-button"]')
     await buttonLogin.click()
     await page.waitForURL('https://vdc-test.xelon.ch/')
-    await page.locator('h1', {hasText: 'Dashboard'})
+    await page.locator('h1', { hasText: 'Dashboard' })
 
-// chech Dshboard
-    await page.locator('.resource-block-note .ml-auto:visible')
-// chech Manage My Organization
-// chech Manage All Organizations
-// chech All Devices
-// chech Disaster Recovery
-// chech Templates & ISOs
-// chech Networking
-// chech Kubernetes
-// chech Persistent Storage
-// chech Databases
-// chech Billing Details
-// chech Usage Overview
-// chech Operational Area
-// chech Sales Dashboard
-// chech Accounting
+    // chech Dshboard
+    // const resourceBlockOnDashbord = page.locator('.resource-block-note').first()
+    const resourceBlockOnDashbord = page.locator('.resource-block-note').nth(0)
+    await expect(resourceBlockOnDashbord).toBeVisible({ timeout: 20000 })
+
+    // chech Manage My Organization
+    await page.locator('#my-organization').click()
+    const buttonCreateUser = page.locator('[data-cy="my-org-add-user"]')
+    await buttonCreateUser.waitFor({
+        state: "visible"
+    })
+    // chech Manage All Organizations
+    await page.locator('#all-organizations').click()
+    const buttonCreateOrganisation = page.locator('[data-cy="create-undefined"]')
+    await buttonCreateOrganisation.waitFor({
+        state: "visible"
+    })
+    // chech All Devices
+    await page.locator('#allDevices').click()
+    const buttonCreateDevice = page.locator('[data-cy="devices-create-new-device-link"]')
+    await buttonCreateDevice.waitFor({
+        state: "visible"
+    })
+    // chech Disaster Recovery
+    await page.locator('#disasterRecovery').click()
+    const buttonCreateResPull = page.locator('[data-cy="create-undefined"]')
+    await buttonCreateResPull.waitFor({
+        state: "visible"
+    })
+    // chech Templates & ISOs
+    // chech Networking
+    // chech Kubernetes
+    // chech Persistent Storage
+    // chech Databases
+    // chech Billing Details
+    // chech Usage Overview
+    // chech Operational Area
+    // chech Sales Dashboard
+    // chech Accounting
 
 
     // await page.locator('[data-cy="header-account-button"]').click()
