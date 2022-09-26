445b3bf97b6622004fc63f0f09e6efcf788b73ab
taran-7 <taran76@gmail.com>
2022-09-26 21:47:04 +0300

    up

--- tests/checkSite.spec.js
+++ tests/checkSite.spec.js
@@ -32,46 +32,6 @@ test('Login,  Log out', async ({ page }) => {
     await buttonCreateUser.waitFor({
         state: "visible"
     })
-    // chech Manage All Organizations
-    await page.locator('#all-organizations').click()
-    const buttonCreateOrganisation = page.locator('[data-cy="create-undefined"]')
-    await buttonCreateOrganisation.waitFor({
-        state: "visible"
-    })
-    // chech All Devices
-    await page.locator('#allDevices').click()
-    const buttonCreateDevice = page.locator('[data-cy="devices-create-new-device-link"]')
-    await buttonCreateDevice.waitFor({
-        state: "visible"
-    })
-    // chech Disaster Recovery
-    await page.locator('#disasterRecovery').click()
-    const buttonCreateResPull = page.locator('[data-cy="create-undefined"]')
-    await buttonCreateResPull.waitFor({
-        state: "visible"
-    })
-    // chech Templates & ISOs
-
-    await page.locator('#disasterRecovery').click()
-    page.locator('[data-cy="create-undefined"]').waitFor({
-        state: "visible"
-    })
 
 
-    // chech Networking
-    // chech Kubernetes
-    // chech Persistent Storage
-    // chech Databases
-    // chech Billing Details
-    // chech Usage Overview
-    // chech Operational Area
-    // chech Sales Dashboard
-    // chech Accounting
-
-
-    // await page.locator('[data-cy="header-account-button"]').click()
-    // await page.locator('[data-cy="header-logout-button"]').click()
-    // await page.waitForURL('https://vdc-test.xelon.ch/login')
-    // const loginForm = page.locator('[data-cy="login-button"]')
-    // await expect(loginForm).toHaveText('Log in');
 });
