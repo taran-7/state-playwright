1f71f93c8b22b96896cc5672302a8c4fef52f7fc
Olex <58403319+taran-7@users.noreply.github.com>
2022-12-08 22:52:47 +0200

    Create randomStateCode.js

---
+++ tests/randomStateCode.js
@@ -0,0 +1,6 @@
+function fakerRandomStateCode() {
+    let stateCodes = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+    let i = Math.floor(Math.random() * stateCodes.length);
+    return stateCodes[i];
+}
+console.log(fakerRandomStateCode());
